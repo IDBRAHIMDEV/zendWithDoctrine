8d487ddbb7be1cb20e174584f06a67fc9cabe6c8
Mohamed <idbrahimdev@gmail.com>
2018-08-09 15:44:18 +0100

    Ajouter les getters and setters in Product entity

--- module/Application/src/Application/Model/Entity/Product.php
+++ module/Application/src/Application/Model/Entity/Product.php
@@ -40,4 +40,76 @@ class Product extends AbstractEntity
         }
     }
 
+     /**
+     * Get the value of id
+     *
+     * @return  integer
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @param  integer  $id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     *
+     * @return  string
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param  string  $name
+     *
+     * @return  self
+     */ 
+    public function setName(string $name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of description
+     *
+     * @return  string
+     */ 
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set the value of description
+     *
+     * @param  string  $description
+     *
+     * @return  self
+     */ 
+    public function setDescription(string $description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
 }
